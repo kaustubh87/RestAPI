e88cbf2ec7b278737bb26aaa3836ddf560adbd78
kvin1087 <kaustubh87@gmail.com>
2020-04-12 08:49:40 -0400

    Added MongoDB connection

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 const authRoutes = require('./routes/auth-routes.js');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const app = express();
+
+
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
+//connect to DB
 
+mongoose.connect(keys.mongodb.dbURI, () => {
+  console.log('Connected to MongoDB');
+});
 var port = process.env.PORT || 3000;
 
 app.use('/auth', authRoutes);
